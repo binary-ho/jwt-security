41539fea17267a096dc01f2cc24ad90267a84c90
Jinho <dfghcvb11@naver.com>
2023-02-05 20:07:26 +0900

    feat: filterChain 설정

---
+++ src/main/java/com/binaryho/jwtsecurity/config/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.binaryho.jwtsecurity.config;
+
+import java.util.LinkedList;
+import java.util.List;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+            .formLogin().disable()
+            .httpBasic().disable()
+            .authorizeRequests()
+            .antMatchers("/api/v1/user/**")
+            .access("hasAnyRole('ROLE_USER', 'ROLE_MANAGER', 'ROLE_ADMIN')")
+
+            .antMatchers("/api/v1/manager/**")
+            .access("hasAnyRole('ROLE_MANAGER', 'ROLE_ADMIN')")
+
+            .antMatchers("/api/v1/admin/**")
+            .access("hasAnyRole('ROLE_ADMIN')")
+            .anyRequest().permitAll();
+
+        return http.build();
+    }
+}
