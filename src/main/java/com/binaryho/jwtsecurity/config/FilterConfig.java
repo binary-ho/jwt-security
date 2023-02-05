a2ec93ce4b913cbdfdb5ae9ae35e23f8247271b8
Jinho <dfghcvb11@naver.com>
2023-02-05 22:28:27 +0900

    feat: 필터 우선순위 확인을 위한 OrderFilters 구현

---
+++ src/main/java/com/binaryho/jwtsecurity/config/FilterConfig.java
@@ -0,0 +1,29 @@
+package com.binaryho.jwtsecurity.config;
+
+import com.binaryho.jwtsecurity.filter.Order0Filter;
+import com.binaryho.jwtsecurity.filter.Order1Filter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FilterConfig {
+
+    @Bean
+    public FilterRegistrationBean<Order0Filter> order0FilterBean() {
+        FilterRegistrationBean<Order0Filter> bean = new FilterRegistrationBean<>(new Order0Filter());
+
+        bean.addUrlPatterns("/*");
+        bean.setOrder(0);
+        return bean;
+    }
+
+    @Bean
+    public FilterRegistrationBean<Order1Filter> order1FilterBean() {
+        FilterRegistrationBean<Order1Filter> bean = new FilterRegistrationBean<>(new Order1Filter());
+
+        bean.addUrlPatterns("/*");
+        bean.setOrder(1);
+        return bean;
+    }
+}
---
+++ src/main/java/com/binaryho/jwtsecurity/filter/Order1Filter.java
@@ -0,0 +1,20 @@
+package com.binaryho.jwtsecurity.filter;
+
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class Order1Filter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+        throws IOException, ServletException {
+
+        System.out.println("Order 1 필터");
+        chain.doFilter(request, response);
+    }
+}
