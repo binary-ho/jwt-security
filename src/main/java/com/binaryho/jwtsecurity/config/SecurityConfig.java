bb3bd21f0935533cc65cddb7b99f43f06747d089
Jinho <dfghcvb11@naver.com>
2023-02-05 21:19:36 +0900

    chore: cors filter 재 import

--- src/main/java/com/binaryho/jwtsecurity/config/SecurityConfig.java
+++ src/main/java/com/binaryho/jwtsecurity/config/SecurityConfig.java
@@ -1,13 +1,13 @@
 package com.binaryho.jwtsecurity.config;
 
 import lombok.RequiredArgsConstructor;
-import org.apache.catalina.filters.CorsFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableWebSecurity
