4cb3cc5d989069b4c713245a488b58e1483c2b4d
Jinho <dfghcvb11@naver.com>
2023-02-13 20:46:57 +0900

    feat : controller에 join을 수행하는 PostMapping method 구현

--- src/main/java/com/binaryho/jwtsecurity/controller/RestApiController.java
+++ src/main/java/com/binaryho/jwtsecurity/controller/RestApiController.java
@@ -1,11 +1,19 @@
 package com.binaryho.jwtsecurity.controller;
 
+import com.binaryho.jwtsecurity.model.User;
+import com.binaryho.jwtsecurity.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class RestApiController {
+
+    private UserRepository userRepository;
 
     @GetMapping("home")
     public String home() {
@@ -15,5 +23,13 @@ public class RestApiController {
     @PostMapping("token")
     public String token() {
         return "<h1>내가 만든 Token~~~~</h1>";
+    }
+
+    @PostMapping("join")
+    public String join(@RequestBody User user) {
+        user.setPassword(user.getPassword());
+        user.setRoles("ROLE_USER");
+        userRepository.save(user);
+        return "회원가입완료";
     }
 }
